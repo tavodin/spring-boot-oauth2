2c554ef75ea641e73b7501b290d6e02a45fa9005
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2024-08-29 15:38:52 -0300

    BCrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$.4pIWe5DbvOpyxcVOLOzdeq5qebWeJMFBnv688zFvDjBeiHhAksH2');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$.4pIWe5DbvOpyxcVOLOzdeq5qebWeJMFBnv688zFvDjBeiHhAksH2');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
